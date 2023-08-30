aa716f35f6bb027c7acb6a39162930ed6edcf5ad
unknown <230450094@kccs.auth>
2023-08-30 13:32:50 +0900

    test

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,5 +1,6 @@
 export default {
-  region: 'ap-northeast-1',
-  accessKeyId: 'AKIAYFREWV74TF3GHQ6N',
-  secretAccessKey: '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'
-};
+    region: 'ap-northeast-1',
+    accessKeyId: 'AKIAYFREWV74TF3GHQ6N',
+    secretAccessKey: '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'
+  };
+  
